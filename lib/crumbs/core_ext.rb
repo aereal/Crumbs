f901cc1b4fea6b3cb4e3f887aff8447732537bfb
aereal <aereal.h@gmail.com>
2010-10-13 17:59:18 +0900

    add Pathname#install, Pathname#/

--- lib/crumbs/core_ext.rb
+++ lib/crumbs/core_ext.rb
@@ -1,7 +1,18 @@
+require "fileutils"
 require "pathname"
 
 class String
 	def to_path
 		Pathname.new(self)
+	end
+end
+
+class Pathname
+	def install(dst)
+		FileUtils.install self.to_s, dst.to_s
+	end
+
+	def /(that)
+		join that.to_s
 	end
 end
